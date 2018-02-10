cb0f60a93fa434e5fdfffa0262e4e4873b22b05a
palburtus <patrick.alburtus@gmail.com>
2018-02-10 17:21:00 -0500

    created empty xamirn project to use as sample app.  Created sha256 hash generator and unit test

---
+++ Com.Grooveip.Api/Com.Grooveip.Sdk/Utils/HashGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Grooveip.Sdk.Utils
+{
+    public class HashGenerator
+    {
+        public static string CreateSHA256(string input)
+        {
+            SHA256Managed hasher = new SHA256Managed();
+            
+            byte[] buffer = hasher.ComputeHash(Encoding.ASCII.GetBytes(input), 0, Encoding.ASCII.GetByteCount(input));
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach(byte b in buffer)
+            {
+                sb.Append(b.ToString("x2"));
+            }
+
+            return sb.ToString();
+
+        }
+    }
+}
---
+++ Com.Grooveip.Api/UnitTests/HashGeneratorTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Com.Grooveip.Sdk.Utils;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class HashGeneratorTests
+    {
+        [TestMethod]
+        public void createSHA256_createsSHA256HashOfAString_returnsHashedString()
+        {
+            string secretKey = "myapiscret";
+            int clientId = 1;
+            string areaCode = "732";
+            string requestId = "c4h5dadb-8241-4765-8fed-845d35bbfe54/hash/293b945730fd13064bcee146989eec92272788a555369947b609a29d5bfd7avv";
+
+            string input = string.Format("{0}{1}{2}{3}", clientId, areaCode, requestId, secretKey);
+
+            Assert.AreEqual("5b7252c27e483d6c92b1565f7386c3d1ec0dcf9f0d444a8760520a673c839528", HashGenerator.CreateSHA256(input));
+
+        }
+    }
+}
