e93f77f373681b0193c0bcb85e41cf2578248be5
palburtus <patrick.alburtus@gmail.com>
2018-02-18 18:29:38 -0500

    Added number reserve request and response objects

---
+++ Com.Grooveip.Api/Com.Grooveip.Sdk/Model/NumberReserveResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Grooveip.Sdk.Model
+{
+    public class NumberReserveResponse
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public int UserId { get; set; }
+        public string UserToken { get; set; }
+    }
+}
