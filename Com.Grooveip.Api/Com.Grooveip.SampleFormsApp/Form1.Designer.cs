81bb8ca1b84109f4b9930369ac1e1c5cfd7114b2
palburtus <patrick.alburtus@gmail.com>
2018-02-19 12:37:28 -0500

    added a windowns forms application to serve as a sample

---
+++ Com.Grooveip.Api/Com.Grooveip.SampleFormsApp/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Com.Grooveip.SampleFormsApp
+{
+    partial class FormNumbersInventory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSearchNumbers = new System.Windows.Forms.Button();
+            this.labelNoNumbers = new System.Windows.Forms.Label();
+            this.listViewNumbersInventory = new System.Windows.Forms.ListView();
+            this.progressBarLoading = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // buttonSearchNumbers
+            // 
+            this.buttonSearchNumbers.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonSearchNumbers.Location = new System.Drawing.Point(0, 409);
+            this.buttonSearchNumbers.Name = "buttonSearchNumbers";
+            this.buttonSearchNumbers.Size = new System.Drawing.Size(297, 29);
+            this.buttonSearchNumbers.TabIndex = 0;
+            this.buttonSearchNumbers.Text = "Search Numbers";
+            this.buttonSearchNumbers.UseVisualStyleBackColor = true;
+            this.buttonSearchNumbers.Click += new System.EventHandler(this.buttonSearchNumbers_Click);
+            // 
+            // labelNoNumbers
+            // 
+            this.labelNoNumbers.AutoSize = true;
+            this.labelNoNumbers.Location = new System.Drawing.Point(95, 188);
+            this.labelNoNumbers.Name = "labelNoNumbers";
+            this.labelNoNumbers.Size = new System.Drawing.Size(111, 13);
+            this.labelNoNumbers.TabIndex = 1;
+            this.labelNoNumbers.Text = "You have no numbers";
+            this.labelNoNumbers.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelNoNumbers.Visible = false;
+            // 
+            // listViewNumbersInventory
+            // 
+            this.listViewNumbersInventory.Dock = System.Windows.Forms.DockStyle.Top;
+            this.listViewNumbersInventory.Location = new System.Drawing.Point(0, 0);
+            this.listViewNumbersInventory.Name = "listViewNumbersInventory";
+            this.listViewNumbersInventory.Size = new System.Drawing.Size(297, 403);
+            this.listViewNumbersInventory.TabIndex = 3;
+            this.listViewNumbersInventory.UseCompatibleStateImageBehavior = false;
+            this.listViewNumbersInventory.View = System.Windows.Forms.View.List;
+            this.listViewNumbersInventory.Visible = false;
+            // 
+            // progressBarLoading
+            // 
+            this.progressBarLoading.Location = new System.Drawing.Point(0, 384);
+            this.progressBarLoading.Name = "progressBarLoading";
+            this.progressBarLoading.Size = new System.Drawing.Size(297, 23);
+            this.progressBarLoading.TabIndex = 4;
+            this.progressBarLoading.Visible = false;
+            // 
+            // FormNumbersInventory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 438);
+            this.Controls.Add(this.progressBarLoading);
+            this.Controls.Add(this.listViewNumbersInventory);
+            this.Controls.Add(this.labelNoNumbers);
+            this.Controls.Add(this.buttonSearchNumbers);
+            this.Name = "FormNumbersInventory";
+            this.Text = "My Numbers";
+            this.Load += new System.EventHandler(this.FormNumbersInventory_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSearchNumbers;
+        private System.Windows.Forms.Label labelNoNumbers;
+        private System.Windows.Forms.ListView listViewNumbersInventory;
+        private System.Windows.Forms.ProgressBar progressBarLoading;
+    }
+}
+
---
+++ Com.Grooveip.Api/Com.Grooveip.SampleFormsApp/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Com.Grooveip.SampleFormsApp
+{
+    public partial class FormNumbersInventory : Form
+    {
+        public FormNumbersInventory()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void FormNumbersInventory_Load(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            InitializeViews();
+            Cursor.Current = Cursors.AppStarting;
+        }
+
+        private void InitializeViews()
+        {
+            Thread.Sleep(10000);
+            labelNoNumbers.Visible = true;
+        }
+
+        private void buttonSearchNumbers_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+    }
+}
