dc35e6181e7aeebf3a19a3aabc1906e174ba334b
palburtus <patrick.alburtus@gmail.com>
2018-04-19 22:32:38 -0400

    added registration resposne

---
+++ Com.Grooveip.Api/Com.Grooveip.Sdk/Model/RegistrationResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Grooveip.Sdk.Model
+{
+    public class RegistrationResponse
+    {
+        public string Token { get; set; }
+        public string Key { get; set; }
+        public int UserId { get; set; }
+    }
+}
