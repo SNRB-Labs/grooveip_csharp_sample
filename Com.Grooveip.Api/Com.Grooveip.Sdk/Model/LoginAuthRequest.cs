244f153cefe4be49f9352516d4ab349d9dd7fd74
palburtus <patrick.alburtus@gmail.com>
2018-04-20 01:07:45 -0400

    added auth prop for requests

--- Com.Grooveip.Api/Com.Grooveip.Sdk/Model/LoginAuthRequest.cs
+++ Com.Grooveip.Api/Com.Grooveip.Sdk/Model/LoginAuthRequest.cs
@@ -10,7 +10,6 @@ namespace Com.Grooveip.Sdk.Model
     {
         public string Email { get; set; }
         public string Password { get; set; }
-
         public string AuthHash { get; set; }
     }
 }
