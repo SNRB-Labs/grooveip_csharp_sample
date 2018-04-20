6c289aec2724be33c62a943f9cc0c3695812affd
palburtus <patrick.alburtus@gmail.com>
2018-04-19 22:30:43 -0400

    added hash property for login request

--- Com.Grooveip.Api/Com.Grooveip.Sdk/Model/LoginAuthRequest.cs
+++ Com.Grooveip.Api/Com.Grooveip.Sdk/Model/LoginAuthRequest.cs
@@ -10,5 +10,7 @@ namespace Com.Grooveip.Sdk.Model
     {
         public string Email { get; set; }
         public string Password { get; set; }
+
+        public string AuthHash { get; set; }
     }
 }
