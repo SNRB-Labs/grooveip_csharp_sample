54a073ce39ffd6478311475478d561a116c9a570
palburtus <patrick.alburtus@gmail.com>
2018-04-22 21:20:49 -0400

    added api url for searching by area code

--- Com.Grooveip.Api/Com.Grooveip.Sdk/Api/ApiClient.cs
+++ Com.Grooveip.Api/Com.Grooveip.Sdk/Api/ApiClient.cs
@@ -9,7 +9,7 @@ namespace Com.Grooveip.Sdk.Api
 {
     public class ApiClient
     {
-        private const string secret = "mysecretkey";
+        private const string secret = "myapisecret";
         private const int clientId = 1;
         private const string baseUrl = "http://staging-commercial.azurewebsites.net/api";
 
@@ -20,6 +20,18 @@ namespace Com.Grooveip.Sdk.Api
             string hash = HashGenerator.CreateSHA256(string.Format("{0}{1}{2}{3}{4}", clientId, page, quantity, requestId, secret));
 
             string url = string.Format("{0}/numbers/inventory/{1}/page/{2}/quantity/{3}/requestId/{4}/hash/{5}", baseUrl, clientId, page, quantity, requestId, hash);
+
+            return url;
+        }
+
+        public static string BuildSearchUrl(string areaCode)
+        {
+            string requestId = GetRequestId();
+            
+            string hash = HashGenerator.CreateSHA256(string.Format("{0}{1}{2}{3}", baseUrl, clientId, areaCode, requestId));
+            
+            string url = HashGenerator.CreateSHA256(string.Format("{0}/numbers/list/{1}" +
+                "/areaCode/{2}/requestId/{3}/hash/{4}", baseUrl, clientId, areaCode, requestId, hash));
 
             return url;
         }
