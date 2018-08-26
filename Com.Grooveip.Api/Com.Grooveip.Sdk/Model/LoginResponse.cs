4f70cef777c570281fc5d0dc871174e3b365c721
patrick.alburtus@gmail.com <patrick.alburtus@gmail.com>
2018-08-26 18:44:01 -0400

    fixed branching issues added simple login request back

---
+++ Com.Grooveip.Api/Com.Grooveip.Sdk/Model/LoginResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Grooveip.Sdk.Model
+{
+    public class LoginResponse
+    {
+        public int UserId;
+        public string Token;
+        public int CustomerId;
+        public string Key;
+    }
+}
