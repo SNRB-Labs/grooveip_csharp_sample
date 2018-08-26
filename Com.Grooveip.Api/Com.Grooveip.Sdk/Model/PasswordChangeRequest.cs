1422a5e68527a0feefe3767d025ffb09edbe3702
patrick.alburtus@gmail.com <patrick.alburtus@gmail.com>
2018-08-26 18:11:07 -0400

    added password request to SDK model

---
+++ Com.Grooveip.Api/Com.Grooveip.Sdk/Model/PasswordChangeRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Grooveip.Sdk.Model
+{
+    public class PasswordChangeRequest
+    {
+        public string Email { get; set; }
+        public string Token { get; set; }
+        public string NewPassword { get; set; }
+        public string NewPasswordConfirm { get; set; }
+        public string Hash { get; set; }
+
+        public bool DoPasswordsMatch()
+        {
+            return NewPassword == NewPasswordConfirm;
+        }
+    }
+}
---
+++ Com.Grooveip.Api/Com.Grooveip.Sdk/Model/PasswordResetRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Grooveip.Sdk.Model
+{
+    public class PasswordResetRequest
+    {
+        public string Email { get; set; }
+        public string Hash { get; set; }
+    }
+}
